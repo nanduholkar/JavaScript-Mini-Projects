3107c9077490acfd02d1cae0ba1f9e95e8cbc26f
nanduholkar <nanduholkar7@gmail.com>
2026-04-10 19:13:44 +0530

    Implemented expense tracker with add/delete functionality, dynamic UI rendering, total calculation and localStorage support

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("App loaded");
 
-  let expenses = []
+  let expenses =JSON.parse(localStorage.getItem("expenses"))  || []
 
+  
   const expenseForm = document.getElementById("expense-form");
   const expenseNameInput = document.getElementById("expense-name");
   const expenseAmountInput = document.getElementById("expense-amount");
   const expenseList = document.getElementById("expense-list");
   const totalAmountDisplay = document.getElementById("total-amount");
 
-  console.log(expenseForm);
-  console.log(expenseNameInput);
-  console.log(expenseAmountInput);
-  console.log(expenseList);
-  console.log(totalAmountDisplay);
+
+  renderExpenses()
+  updateTotal()
+
+  
 
   expenseForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -27,11 +28,26 @@ document.addEventListener("DOMContentLoaded", () => {
         amount: amount,
       }
       expenses.push(newExpense)
+      saveExpensesToLocal()
 
       renderExpenses()
+      updateTotal()
+      // clearing inputs 
+      expenseAmountInput.value =""
+      expenseNameInput.value = ""
 
     }
-    else{ return }
+  })
+  expenseList.addEventListener("click", (e) =>{
+    if(e.target.tagName === "BUTTON"){
+      const expenseId = parseInt(e.target.getAttribute('data-id'))
+      expenses = expenses.filter((expenses) => expenses.id !== expenseId)
+      renderExpenses()
+      updateTotal()
+      saveExpensesToLocal()
+      
+    }
+    
 
     
   });
@@ -39,8 +55,25 @@ document.addEventListener("DOMContentLoaded", () => {
     expenseList.innerHTML=""
     expenses.forEach((expense) => {
       const li = document.createElement("li")
-      li.innerHTML = `${expense.name} - $${expense.amount} <button data-id = "${expense.id}>Delete</button>`
+      li.innerHTML = `${expense.name} - $${expense.amount} <button data-id = "${expense.id}">Delete</button>`
       expenseList.appendChild(li);
+
+
     })
+
+
+    
+    
+  }
+  function updateTotal(){
+    var totalAmount = 0
+    expenses.forEach((expenses) => {
+      totalAmount = totalAmount + expenses.amount
+    })
+    totalAmountDisplay.textContent = totalAmount.toFixed(2)
+  }
+
+  function saveExpensesToLocal(){
+    localStorage.setItem("expenses",JSON.stringify(expenses))
   }
 });
